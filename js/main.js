140ec1979792cc88dd64104392a837f7a0127f12
vetrimaniyan <69971864+vetrimaniyan@users.noreply.github.com>
2020-08-21 13:33:21 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -252,7 +252,7 @@
 
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Feb 05, 2020 09:30:00").getTime();
+		var countDownDate = new Date("Sep 16, 2020 06:30:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
